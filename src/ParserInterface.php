b0b2cd7414287f185d2f30733d15298271339e8c
Alexander Muratov <muratoffalex@gmail.com>
2018-08-11 14:09:20 +0300

    Init commit

---
+++ src/ParserInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace muratoffalex\parser;
+
+/**
+ * @author Victor Zinchenko <zinchenko.us@gmail.com>
+ */
+interface ParserInterface
+{
+
+    /**
+     * @param string $url
+     * @param string $tag
+     * @return array
+     */
+    public function process(string $url, string $tag): array;
+}
